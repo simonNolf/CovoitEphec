d532d655717beff96cd62f567a02be514972080b
simonNolf <simon.nolf@gmail.com>
2024-04-12 21:47:45 +0200

    DB

--- DB/init.sql
+++ DB/init.sql
@@ -1,22 +1,18 @@
--- Création de la base de données TFE
-CREATE DATABASE IF NOT EXISTS "TFE"
-    WITH
-    OWNER = postgres
-    ENCODING = 'UTF8'
-    LC_COLLATE = 'French_Belgium.1252'
-    LC_CTYPE = 'French_Belgium.1252'
-    TEMPLATE = template0;
+-- Table: public.user
 
--- Changement du search_path vers le schéma public
-SET search_path = public;
+-- DROP TABLE IF EXISTS public."user";
 
--- Création de la table "user"
-CREATE TABLE IF NOT EXISTS "user"
+CREATE TABLE IF NOT EXISTS public."user"
 (
-    matricule character varying(8) NOT NULL,
-    password character varying NOT NULL,
-    salt character varying NOT NULL,
-    status character varying NOT NULL DEFAULT 'pending',
+    matricule character varying(8) COLLATE pg_catalog."default" NOT NULL,
+    password character varying COLLATE pg_catalog."default" NOT NULL,
+    salt character varying COLLATE pg_catalog."default" NOT NULL,
+    status character varying COLLATE pg_catalog."default" NOT NULL DEFAULT 'pending'::character varying,
     activation_expiration timestamp with time zone NOT NULL,
     CONSTRAINT user_pkey PRIMARY KEY (matricule)
-);
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS public."user"
+    OWNER to postgres;
