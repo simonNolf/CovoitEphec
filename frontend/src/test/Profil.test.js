63ae3b9b561de5509da946df5c5fade5d221e2b5
simonNolf <simon.nolf@gmail.com>
2024-05-03 10:29:23 +0200

    test fix

--- frontend/src/test/Profil.test.js
+++ frontend/src/test/Profil.test.js
@@ -1,14 +1,43 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import { BrowserRouter as Router } from 'react-router-dom'; // Importez le bon Router
+
 import Profil from '../Profil';
 
 describe('Profil component', () => {
   test('renders profile page', () => {
-    render(<Profil />);
+    render(
+      <Router>
+        <Profil />
+      </Router>
+    );
     const headingElement = screen.getByRole('heading', { name: /Page de profil/i });
     expect(headingElement).toBeInTheDocument();
 
-    const paragraphElement = screen.getByText(/Aucune donnée d'utilisateur trouvée./i);
+    const paragraphElement = screen.getByText(/Vous devez vous connecter pour accéder à cette page./i);
     expect(paragraphElement).toBeInTheDocument();
+  });
+  test('renders profile page with user data', () => {
+    const user = {
+      nom: 'Doe',
+      prenom: 'John',
+      adresse: '123 Rue de la Test',
+      matricule: '12345'
+    };
+    sessionStorage.setItem('matricule', '12345');
+    sessionStorage.setItem('token', 'test-token');
+    
+    render(
+      <Router>
+        <Profil />
+      </Router>
+    );
+
+    const headingElement = screen.getByRole('heading', { name: /Page de profil/i });
+    expect(headingElement).toBeInTheDocument();
+
+    const nomElement = screen.getByText(/Matricule: 12345/i);
+    expect(nomElement).toBeInTheDocument();
+    
   });
 });
